49e41a2957fcb08caa7750b617642e2f95a1b441
Muftau Lawal <muflawal@gmail.com>
2024-12-14 11:18:34 +0000

    add changes

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -1,8 +1,32 @@
 const GreeterContract = artifacts.require("Greeter");
 
-writeContracts("Greeter", () => {
+contract("Greeter", () => {
   it("has been deployed successfully", async () => {
     const greeter = await GreeterContract.deployed();
-    assert(greeter, "contract was not deployed");
+    assert(greeter, "Contract failed to deploy");
+  });
+
+  describe("greet()", () => {
+    it("returns 'Hello, World!'", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hello, World!";
+      const actual = await greeter.greet();
+
+      assert.equal(actual, expected, "greeted with 'Hello, World!'");
+    });
+  });
+});
+
+contract("Greeter: update greeting", () => {
+  describe("setGreeting(string)", () => {
+    it("sets greeting to passed in string", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hi there!";
+
+      await greeter.setGreeting(expected);
+      const actual = await greeter.greet();
+
+      assert.equal(actual, expected, "greeted was not updated");
+    });
   });
 });
